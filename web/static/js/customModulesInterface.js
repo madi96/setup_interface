2777ca58a37981fb617ec356c2e4b12436215a0c
Yaakoubi <yaakoubi.oussema@gmail.com>
2019-03-14 13:39:44 +0100

    javascripts:: adding changes in smiley

--- web/static/js/customModulesInterface.js
+++ web/static/js/customModulesInterface.js
@@ -1,6 +1,11 @@
 var joystickDefaultTransform;
 var leverDefaultTransform;
 var rewardDefaultTransform;
+var smiling;
+var happy;
+var confused;
+var sad;
+var unhappy;
 function setToDefault(){
     svgDocument = document.getElementById("svgInterface").contentDocument;
     // Get default jostick transform
@@ -14,6 +19,23 @@ function setToDefault(){
     rewardDefaultTransform =  reward.getAttribute('transform')
     
 }
+
+function setSmileysToDefault(){
+		svgDocument = document.getElementById("svgInterface").contentDocument;
+		smiling = svgDocument.getElementById("svgInterface").getElementById('smiling');
+		happy = svgDocument.getElementById("svgInterface").getElementById('happy');
+ 		confused = svgDocument.getElementById("svgInterface").getElementById('confused');
+		sad = svgDocument.getElementById("svgInterface").getElementById('sad');
+		unhappy = svgDocument.getElementById("svgInterface").getElementById('unhappy');
+		
+		
+		//set to default
+		smiling.setAttribute("visibility", "hidden");
+		happy.setAttribute("visibility", "hidden");
+		confused.setAttribute("visibility", "visible");
+		sad.setAttribute("visibility", "hidden");
+		unhappy.setAttribute("visibility", "hidden");
+ 	}
 
 window.onload = function() {
     var ws = new WebSocket("ws://localhost:8888/ws");
@@ -59,9 +81,24 @@ window.onload = function() {
 	}
 
 	else if (type == "reward"){
+		var translate_value = infoList[1];
 	    var reward = svgDocument.getElementById("svgInterface").getElementById('reward');
-	    var newState = rewardDefaultTransform +'translate('+(infoList[1]*0).toString()+','+(infoList[1]*-310).toString()+')';
+	    var newState = rewardDefaultTransform +'translate('+(translate_value*0).toString()+','+(translate_value*-310).toString()+')';
 	    reward.setAttribute('transform', newState);
+	    setSmileysToDefault();
+	    if (translate_value >= -1 && translate_value < -0.5){
+			confused.setAttribute("visibility", "hidden");
+			unhappy.setAttribute("visibility", "visible");
+		} else if  (translate_value >= -0.5 && translate_value < 0){
+			confused.setAttribute("visibility", "hidden");
+			sad.setAttribute("visibility", "visible");
+		} else if  (translate_value > 0 && translate_value < 0.5){
+			confused.setAttribute("visibility", "hidden");
+			smiling.setAttribute("visibility", "visible");
+		} else if  (translate_value >= 0.5 && translate_value <= 1){
+			confused.setAttribute("visibility", "hidden");
+			happy.setAttribute("visibility", "visible");
+		}
 	}
     }
 };
